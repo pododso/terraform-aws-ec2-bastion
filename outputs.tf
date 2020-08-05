55d0a7018e31e4ac8b406748f15cd04f0618beb6
Paolo Cadiz <pscadiz@quadx.xyz>
2020-08-05 19:45:24 +0800

    added security_group_id output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   description = "Public IP address of the instance"
   value       = aws_instance.instance.public_ip
 }
+
+output "security_group_id" {
+  description = "Security Group ID used by the instance"
+  value       = aws_security_group.security_group.id
+}
